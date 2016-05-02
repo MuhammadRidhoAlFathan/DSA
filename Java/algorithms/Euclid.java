fedfa0ab2087d83baacf61591ecf9ea964b66cc9
Animesh <ani420mkjee@gmail.com>
2016-05-02 11:36:04 +0530

    Committing Algorithms

---
+++ Java/algorithms/Euclid.java
@@ -0,0 +1,14 @@
+class Euclid{
+  public static int euclid(int p, int q){
+    //System.out.printf("%d,%d",p,q);
+    if (q==0){
+      return p;
+    }
+    else {
+      return euclid(q,p%q);
+    }
+  }
+  public static void main(String args[]){
+    System.out.println(Euclid.euclid(12,5));
+  }
+}
---
+++ Java/algorithms/readme.txt
@@ -0,0 +1,3 @@
+The files in this folder are created following the book Algorithms, By Robert Sedgewick and
+Keving Wyne.
+The programs included shall have the question commented in them and the solution followed.
